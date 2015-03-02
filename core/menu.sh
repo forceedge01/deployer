9012b6535602abbe674b864d1601a0e2b0ce1b45
forceedge <wahab.qureshi@digitalanimal.com>
2015-03-02 00:11:21 +0000

    Updated menu

--- core/menu.sh
+++ core/menu.sh
@@ -11,8 +11,9 @@ function deployMenu() {
 	printMenu "ssh" "log into ssh machine"
 	printMenu "ssh [arg]" "ssh commands over to ssh server"
 	printMenu "sshp [arg]" "ssh commands over to ssh server on the project directory"
-	printMenu "deploy" "Deploy latest master branch on remote server"
+	printMenu "deploy | d" "Deploy latest master branch on remote server"
 	printMenu "deploy [branch/commit/tag]" "deploy a branch, version or commit to the remote server"
+	printMenu "remote" "Show remote project version"
 	printMenu "remote init | clone" "Initialize repository on ssh server"
 	printMenu "remote reclone" "Re-Initialize repository on ssh server"
 	printMenu "remote update" "update remote server"
@@ -22,6 +23,7 @@ function deployMenu() {
 	printMenu "config edit" "edit the config file for deployer"
 	printMenu "update" "update deployer"
 	printMenu "open | web" "Open project link in browser"
+	printMenu "version | v" "Deisplay deployer version"
 	printMenu "uninstall" "uninstall deployer"
 }
 
