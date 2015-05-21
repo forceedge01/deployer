e4bd79ccdeeadcf9ff0a852cd27e44d9ad7123b3
wahab qureshi <wahab.qureshi@digitalanimal.com>
2015-05-21 12:12:59 +0100

    Init file now puts all deployer stuff in a separate file and imports that file in the bashrc file, cleaner

--- init.sh
+++ init.sh
@@ -4,13 +4,20 @@
 # check if alias exists, if not add it.
 
 declare deployerAlias="dep"
+declare auxilary='~/.bashrc_deployer_auxilary'
+declare mainFile='~/.bashrc'
 
 currentDirectory=$(pwd)
 
 if [[ ! -f $currentDirectory/init.sh ]]; then
 	echo 'This script needs to run from inside the deployer folder, aborting...'
+elif [[ ! -f $mainFile ]]; then
+	echo "$mainFile not found!"
 else
 	echo 'Checking for deployer installation...'
+
+	echo "Creating $auxilary file"
+	sudo touch $auxilary
 
 	if [[ ! -f /usr/bin/$deployerAlias ]]; then
 		echo 'Sorting out file permissions...'
@@ -26,10 +33,10 @@ else
 		fi
 
 		echo 'Adding alias reload to bashrc'
-		echo 'alias reload="source ~/.bash_profile"' >> ~/.bashrc
+		echo 'alias reload="source ~/.bash_profile"' >> $auxilary
         
         echo 'Adding alias project to bashrc'
-        echo "alias project=\"reload && cd \$(IFS=' ' read -ra chunks <<< $($deployerAlias p); echo ${chunks[3]})\"" >> ~/.bashrc
+        echo "alias project=\"reload && cd \$(IFS=' ' read -ra chunks <<< $($deployerAlias p); echo ${chunks[3]})\"" >> $auxilary
 
         # Change the alias inside files so it works
         sed -i'.bk' s/deployerAlias=.*/deployerAlias=$deployerAlias/ "$currentDirectory/core/loader.sh"
@@ -43,15 +50,18 @@ else
 			echo 'Supported OS';;
 		"linux"* )
 			echo 'Supported OS, adding aliases to bashrc file to normalise deployer environment'
-			echo '# Deployer aliases' >> ~/.bashrc
-			echo "alias $deployerAlias='bash $deployerAlias'" >> ~/.bashrc
-			echo '# End of deployer aliases and functions' >> ~/.bashrc
+			echo '# Deployer aliases' >> $auxilary
+			echo "alias $deployerAlias='bash $deployerAlias'" >> $auxilary
+			echo '# End of deployer aliases and functions' >> $auxilary
 			echo 'Make sure the bashrc file is sourced before using the deployer command';;
 		* )
 			echo 'Unsupported OS';;
 	esac
 
-	source ~/.bashrc
+	echo "Adding $auxilary file path to $mainFile";
+	echo "source $auxilary" >> $mainFile
+
+	source $mainFile
 	echo "Use the '$deployerAlias' command to get started..."
 fi
 echo 'Done.'
