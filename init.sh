27e9b1343b2d6535923363446cf804668252bca6
wahab qureshi <wahab.qureshi@digitalanimal.com>
2015-05-21 12:33:45 +0100

    Fixed issue

--- init.sh
+++ init.sh
@@ -4,15 +4,15 @@
 # check if alias exists, if not add it.
 
 declare deployerAlias="dep"
-declare auxilary='~/.bashrc_deployer_auxilary'
-declare mainFile='~/.bashrc'
+declare auxilary="${HOME}/.bashrc_deployer_auxilary"
+declare mainFile="${HOME}/.bashrc"
 
 currentDirectory=$(pwd)
 
 if [[ ! -f $currentDirectory/init.sh ]]; then
 	echo 'This script needs to run from inside the deployer folder, aborting...'
 elif [[ ! -f $mainFile ]]; then
-	echo "$mainFile not found!"
+	echo "$mainFile file not found!"
 else
 	echo 'Checking for deployer installation...'
 
@@ -59,9 +59,9 @@ else
 	esac
 
 	echo "Adding $auxilary file path to $mainFile";
-	echo "source $auxilary" >> $mainFile
+	echo "source $auxilary" >> "$mainFile"
 
-	source $mainFile
+	source "$mainFile"
 	echo "Use the '$deployerAlias' command to get started..."
 fi
 echo 'Done.'
